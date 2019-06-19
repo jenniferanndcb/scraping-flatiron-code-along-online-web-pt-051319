0e0ea909c8ab69cb8bcf09c6eae1270183e7ea8f
Jen <jenniferanndcb@gmail.com>
2019-06-19 11:46:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
   end 
   
   def get_courses 
-    self.get_page
+    doc = self.get_page.css(".post")
     doc.css(".post").first.css("h2").text
     doc.css(".post").first.css(".date").text
     doc.css(".post").first.css("p").text
